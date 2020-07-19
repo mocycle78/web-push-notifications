6eb27e5d95c50df9c93cc84e95d7e8fac242a71f
BlueAdmin <blueadmin@BLUE2016>
2020-07-18 23:28:35 -0500

    Added the keys that I generated vs what was in the course

--- server/publisher.js
+++ server/publisher.js
@@ -2,21 +2,21 @@ const webPush = require('web-push');
 const faker = require('faker');
 
 const pushSubscription = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/cgpM2kf-o9I:APA91bGsFxoaA-YvOzeMUtmn3BIiWotLkpU3cwZNXOOBNGUJ9e1U6IIHd-oLnsgxFnW2GH6UnBeiXflxPUXmkBl_kk_vkL-oUHMFAOEwKUUsA4Zr9x8x6G9Weu9OnGIW94lJw5YEHYDc",
+    "endpoint": "https://fcm.googleapis.com/fcm/send/d_qM5u3ma7s:APA91bGha4864azPz_N1DgJWmUaJtelWOdVVA_7CxBP5zJoktuc_fH6hdVbIajErr0Od2PJC3UtsLuoojQG22QKlL1-N1JZwNmHI_bnYuNe6umVKyR1JLiYcGgpZm7rI0TpNMrQEuXNI",
     "expirationTime": null,
     "keys": {
-        "p256dh": "BCUxEO-k-xwq8TzNYrvVbvMGjJS2tEQWfZCt6pvbN1OznFSxdTbFCGOgcNfpVWo2imU8ybRamYKz5xVUCJs43do",
-        "auth": "JutsLAPMx6UcqHQrRkm6JQ"
+        "p256dh": "BPDp92oAt2hgg4wJusTIpMX3qvcmPUfc2ixR-slItAzGqAES0Vw5g-uGcnIfXoBO4Itv4UlozLCZhmkYaCkhzRg",
+        "auth": "pngwhovx3evhNhxTIYQSCg"
     }
 }
 
-const vapidPublicKey = 'BBjDHF8cFoJznds83RrbZByg-UQCxWTNb9afVNjxjbpIsj2CaJyKDrndm9GwHk9Tbargfzt83gYprv9kl1OvIEU';
-const vapidPrivateKey = 'j1bKaA0HcYpB58PW3Ho2wg2WcK7raVplzN7zGo3Pnfw';
+const vapidPublicKey = 'BJmcshoxKr9b55-mYsvRPBmdJHISiArTp7fU88FIKKlUERbfS3m_9UUxwDoLHv0O9GICclyBdUAOj2mTJrGJnuw';
+const vapidPrivateKey = '0l2AnT8Ummdy_yttA7fH_TJD6b29AYhLBux3xLEehqY';
 
 const options = {
     TTL: 60,
     vapidDetails: {
-        subject: 'mailto: pushers@pushy.com',
+        subject: 'mailto: mocycle78@yahoo.com',
         publicKey: vapidPublicKey,
         privateKey: vapidPrivateKey
     }
